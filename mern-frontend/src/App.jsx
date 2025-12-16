f744e197233cd8d81814d78e8de55f78f69a366c
Anirudh N <anirudhnarayanan8@gmail.com>
2025-12-16 01:48:16 +0000

    React Folder Creation

---
+++ mern-frontend/src/App.jsx
@@ -0,0 +1,11 @@
+function App() {
+  return (
+    <div className="h-screen flex items-center justify-center bg-gray-900">
+      <h1 className="text-4xl font-bold text-white border border-red-500 p-5 rounded-xl shadow-lg">
+        Tailwind v4 is Working!
+      </h1>
+    </div>
+  );
+}
+
+export default App;
