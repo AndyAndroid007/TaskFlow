14eaa4593d34e7f33cbe904b19ccde36652cdfba
Anirudh <anirudhnarayanan8@gmail.com>
2025-12-16 07:49:13 +0530

    Package Updations

--- mern-frontend/vite.config.js
+++ mern-frontend/vite.config.js
@@ -1,5 +1,6 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react-swc'
+import tailwindcss from '@tailwindcss/vite'
 
 export default defineConfig({
   server: {
@@ -7,5 +8,5 @@ export default defineConfig({
     port: 5173,
     allowedHosts: true
   },
-  plugins: [react()],
+  plugins: [react(), tailwindcss()],
 })
